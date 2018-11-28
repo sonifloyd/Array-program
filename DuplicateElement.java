85f48393aa2dbbc1251d6b849bf6fad3f58bb885
soni3073@gmail.com <soni3073@gmail.com>
2018-11-28 21:36:54 +0530

    duplicate element

---
+++ DuplicateElement.java
@@ -0,0 +1,21 @@
+
+
+/*WAJP TO FIND DUPLICATE ELEMENT IN AN ARRAY*/
+public class DuplicateElement{
+    public static void main(String[] args) {
+        int a[]={ 1,2,3,4,5,3,2,5};
+        int n=8;
+         int duplicate =0;
+        for( int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(a[i]==a[j]){
+                    duplicate=a[i];
+                    System.out.println("Dublicate element is"+duplicate);
+                }
+            
+            }
+            
+        }
+        
+    }
+}
